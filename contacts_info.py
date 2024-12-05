284a24c56e97870fd0b5306af28f92196ecaa95d
sakibcd-097 <sakibcd097@gmail.com>
2024-12-05 19:11:18 +0800

    contacts file updated

--- contacts_info.py
+++ contacts_info.py
@@ -3,7 +3,7 @@ contacts_info = {}
 
 def add_contact(name, email, phone, address):
     if phone in contacts_info:
-        return f"Error: The phone number {phone} already exists."
+        return f"Warning! The phone number {phone} already exists."
     contacts_info[phone] = {"name": name, "email": email, "address": address}
     return f"Contact for {name} added successfully."
 
@@ -20,4 +20,4 @@ def remove_contact(phone):
     if phone in contacts_info:
         del contacts_info[phone]
         return f"Contact with phone number {phone} removed."
-    return f"Error: Contact with phone number {phone} not found."
+    return f"Warning: Contact with phone number {phone} not found."
